bc53fc9a7ccbd690992fba9ed5a110c35ad8b111
Stefan Sundin <git@stefansundin.com>
2016-03-20 16:51:40 -0700

    v0.0.2

--- make-zip-mac.sh
+++ make-zip-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
-V=$(cat extension/manifest.json | grep '"version"' | grep -o "\d+\.\d+\.\d+")
+V=$(cat extension/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
 coffeebar popup.coffee -o extension/js/popup.js
 
 rm -f "privatkopiera-$V.zip"
-zip -r "privatkopiera-$V.zip" extension -x '*.gitkeep' -x '*.DS_Store' -x 'Thumbs.db'
+zip -r "privatkopiera-$V.zip" extension -x '*.gitkeep' -x '*.DS_Store' -x '*Thumbs.db'
