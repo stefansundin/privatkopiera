c645d99f182811a79edef1bfa2395fe61c929c89
Stefan Sundin <git@stefansundin.com>
2016-04-03 16:19:52 -0700

    Create the zip file like the xpi file.

--- make-zip-linux.sh
+++ make-zip-linux.sh
@@ -3,4 +3,5 @@ V=$(cat extension/manifest.json | grep '"version"' | grep -oP "\d+\.\d+\.\d+")
 coffeebar popup.coffee -o extension/js/popup.js
 
 rm -f "privatkopiera-$V.zip"
-zip -r "privatkopiera-$V.zip" extension -x '*.git*' -x '*.DS_Store' -x '*Thumbs.db'
+cd extension
+zip -r "../privatkopiera-$V.zip" . -x '*.git*' -x '*.DS_Store' -x '*Thumbs.db'
