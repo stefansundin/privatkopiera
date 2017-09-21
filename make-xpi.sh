fe3005707e1ce17e311fe0bb92b568c05a73c593
Stefan Sundin <git@stefansundin.com>
2017-09-20 21:11:25 -0700

    Automatically remove console.log lines from Firefox distribution. They complain about that every time.

--- make-xpi.sh
+++ make-xpi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 cp -r extension/{js,img,css,popup.html} firefox
+find firefox -type f -name '*.js' | xargs sed -i.bak '/console\.log/d'
 
 V=$(cat firefox/manifest.json | jq -Mr .version)
 rm -f "privatkopiera-$V.xpi"
 cd firefox
-zip -r "../privatkopiera-$V.xpi" . -x '*.git*' -x '*.DS_Store' -x '*Thumbs.db'
+zip -r "../privatkopiera-$V.xpi" . -x '*.git*' -x '*.DS_Store' -x '*Thumbs.db' -x '*.bak'
 
 diff ../extension/manifest.json manifest.json
