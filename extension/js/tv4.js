8d4bcf4c326d0de21987e9caa193c16b15415c7b
Stefan Sundin <git@stefansundin.com>
2018-06-17 18:15:47 -0700

    Fix tv4.

--- extension/js/tv4.js
+++ extension/js/tv4.js
@@ -2,20 +2,28 @@
 // Example URL:
 // https://www.tv4.se/nyhetsmorgon/klipp/n%C3%A4men-h%C3%A4r-sover-peter-dalle-under-tommy-k%C3%B6rbergs-framtr%C3%A4dande-3349622
 // Data URL:
-// https://prima.tv4play.se/api/web/asset/3349622/play.json?protocol=HLS3
+// https://playback-api.b17g.net/asset/3349622?service=tv4&device=browser&drm=&protocol=hls%2Cdash
+// https://playback-api.b17g.net/media/3349622?service=tv4&device=browser&protocol=hls%2Cdash&is_clip=true
 //
 // TV4 Play
 // Example URL:
 // https://www.tv4play.se/program/nyheterna/3349759
 // Data URL:
-// https://prima.tv4play.se/api/web/asset/3349759/play.json?protocol=HLS3
+// https://playback-api.b17g.net/asset/3349759?service=tv4&device=browser&drm=widevine&protocol=hls%2Cdash
+// https://playback-api.b17g.net/media/3349759?service=tv4&device=browser&protocol=hls%2Cdash&drm=widevine&is_clip=true
+//
+// could probably avoid the request to /asset/ and just grab the tab title for the filename.
+// drm and is_clip seem to be optional:
+// https://playback-api.b17g.net/asset/3349759?service=tv4&device=browser&protocol=hls%2Cdash
+// https://playback-api.b17g.net/media/3349759?service=tv4&device=browser&protocol=hls%2Cdash
 //
 // Multiple items:
 // https://www.tv4play.se/program/jul-med-ernst/3946707
-// https://prima.tv4play.se/api/web/asset/3946707/play.json?protocol=HLS3
+// https://playback-api.b17g.net/asset/3946707?service=tv4&device=browser&protocol=hls%2Cdash
+// https://playback-api.b17g.net/media/3946707?service=tv4&device=browser&protocol=hls%2Cdash
 
 
-function tv4play_callback() {
+function tv4play_asset_callback() {
   console.log(this)
   if (this.status != 200) {
     api_error(this.responseURL, this.status)
@@ -23,53 +31,44 @@ function tv4play_callback() {
   }
 
   var data = JSON.parse(this.responseText)
-  update_filename(`${data.playback.title}.mp4`)
+  update_filename(`${data.metadata.title}.mp4`)
 
-  var streams = []
-  var items = data.playback.items.item
-  if (!(items instanceof Array)) {
-    items = [items]
+  var media_url = `https://playback-api.b17g.net${data.mediaUri}`
+  console.log(media_url)
+  var xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", tv4play_media_callback)
+  xhr.open("GET", media_url)
+  xhr.send()
+}
+
+function tv4play_media_callback() {
+  console.log(this)
+  if (this.status != 200) {
+    api_error(this.responseURL, this.status)
+    return
   }
-  items.forEach(function(item) {
-    console.log(item.mediaFormat)
-    if (item.mediaFormat == "wvm" || item.mediaFormat == "mp4" || item.mediaFormat == "webvtt") {
-      streams.push({
-        url: item.url,
-        mediaFormat: item.mediaFormat,
-      })
-    }
-  })
 
+  var data = JSON.parse(this.responseText)
   var dropdown = $("#streams")
-  var order = "wvm,mp4,webvtt".split(",")
-  streams.sort(function(a,b) {
-    return order.indexOf(a.mediaFormat) > order.indexOf(b.mediaFormat)
-  })
-  .forEach(function(stream) {
-    var option = document.createElement("option")
-    option.value = stream.url
-    option.appendChild(document.createTextNode(stream.mediaFormat))
-    if (stream.mediaFormat == "webvtt") {
-      option.appendChild(document.createTextNode(" (undertexter)"))
-    }
-    dropdown.appendChild(option)
-  })
+  var option = document.createElement("option")
+  option.value = data.playbackItem.manifestUrl
+  option.appendChild(document.createTextNode(data.playbackItem.type))
+  dropdown.appendChild(option)
 
   update_cmd()
-  console.log(url)
 }
 
 matchers.push({
   re: /^https?:\/\/(?:www\.)?tv4(?:play)?\.se\/.*(?:-|\/)(\d+)/,
   func: function(ret) {
     var video_id = ret[1]
-    var data_url = `https://prima.tv4play.se/api/web/asset/${video_id}/play.json?protocol=HLS3`
+    var data_url = `https://playback-api.b17g.net/asset/${video_id}?service=tv4&device=browser&protocol=hls%2Cdash`
     update_filename(`${video_id}.mp4`)
     $("#open_json").href = data_url
 
     console.log(data_url)
     var xhr = new XMLHttpRequest()
-    xhr.addEventListener("load", tv4play_callback)
+    xhr.addEventListener("load", tv4play_asset_callback)
     xhr.open("GET", data_url)
     xhr.send()
   }
