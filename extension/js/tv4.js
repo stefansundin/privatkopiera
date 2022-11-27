735f3e585a3373c7deba6a8200f5ae6966526fd4
Stefan Sundin <git@stefansundin.com>
2022-11-26 17:49:34 -0800

    Update domain used by TV4. Fixes tv4.se and hopefully tv4play.se. Fixes #138.

--- extension/js/tv4.js
+++ extension/js/tv4.js
@@ -1,34 +1,20 @@
 // TV4
 // Example URL:
-// https://www.tv4.se/nyhetsmorgon/klipp/n%C3%A4men-h%C3%A4r-sover-peter-dalle-under-tommy-k%C3%B6rbergs-framtr%C3%A4dande-3349622
+// https://www.tv4.se/klipp/va/3349622/namen-har-sover-peter-dalle-under-tommy-korbergs-framtradande
 // Data URL:
-// https://playback-api.b17g.net/asset/3349622?service=tv4&device=browser&drm=&protocol=hls%2Cdash
-// https://playback-api.b17g.net/media/3349622?service=tv4&device=browser&protocol=hls%2Cdash&is_clip=true
+// https://playback2.a2d.tv/asset/3349622?service=tv4&device=browser&browser=GoogleChrome&protocol=hls%2Cdash&drm=widevine&capabilities=live-drm-adstitch-2%2Cexpired_assets
+// https://playback2.a2d.tv/play/3349622?service=tv4&device=browser&browser=GoogleChrome&protocol=hls%2Cdash&drm=widevine&capabilities=live-drm-adstitch-2%2Cexpired_assets
 //
 // TV4 Play
 // Example URL:
-// https://www.tv4play.se/program/nyheterna/3349759
+// https://www.tv4play.se/program/nyheterna/d%C3%B6dssiffran-stiger-dramatiskt-efter-jordskalv/3349759
 // Data URL:
-// https://playback-api.b17g.net/asset/3349759?service=tv4&device=browser&drm=widevine&protocol=hls%2Cdash
-// https://playback-api.b17g.net/media/3349759?service=tv4&device=browser&protocol=hls%2Cdash&drm=widevine&is_clip=true
-//
-// could probably avoid the request to /asset/ and just grab the tab title for the filename.
-// is_clip seem to be optional:
-// https://playback-api.b17g.net/asset/3349759?service=tv4&device=browser&protocol=hls%2Cdash
-// https://playback-api.b17g.net/media/3349759?service=tv4&device=browser&protocol=hls%2Cdash
-//
-// drm is required:
-// https://www.tv4play.se/program/maria-lang/10001026
-// https://playback-api.b17g.net/asset/10001026?service=tv4&device=browser&drm=widevine&protocol=hls%2Cdash
-//
-// Multiple items:
-// https://www.tv4play.se/program/jul-med-ernst/3946707
-// https://playback-api.b17g.net/asset/3946707?service=tv4&device=browser&protocol=hls%2Cdash
-// https://playback-api.b17g.net/media/3946707?service=tv4&device=browser&protocol=hls%2Cdash
+// https://playback2.a2d.tv/asset/3349759?service=tv4&device=browser&browser=GoogleChrome&protocol=hls%2Cdash&drm=widevine&capabilities=live-drm-adstitch-2%2Cexpired_assets
+// https://playback2.a2d.tv/play/3349759?service=tv4&device=browser&browser=GoogleChrome&protocol=hls%2Cdash&drm=widevine&capabilities=live-drm-adstitch-2%2Cexpired_assets
 
 function tv4play_asset_callback(data) {
   update_filename(`${data.metadata.title.trim()}.mkv`);
-  const media_url = `https://playback-api.b17g.net${data.mediaUri}`;
+  const media_url = `https://playback2.a2d.tv${data.mediaUri}`;
   console.log(media_url);
   fetch(media_url).then(get_json).then(tv4play_media_callback).catch(api_error);
 }
@@ -46,7 +32,7 @@ matchers.push({
   re: /^https?:\/\/(?:www\.)?tv4(?:play)?\.se\.?\/.*(?:-|\/)(\d+)/,
   func: function(ret) {
     const video_id = ret[1];
-    const data_url = `https://playback-api.b17g.net/asset/${video_id}?service=tv4&device=browser&drm=widevine&protocol=hls%2Cdash`;
+    const data_url = `https://playback2.a2d.tv/asset/${video_id}?service=tv4&device=browser&browser=GoogleChrome&protocol=hls%2Cdash&drm=widevine&capabilities=live-drm-adstitch-2%2Cexpired_assets`;
     update_filename(`${video_id}.mkv`);
     update_json_url(data_url);
 
