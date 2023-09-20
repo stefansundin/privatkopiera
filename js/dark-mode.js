adea5c118705cbeefb44ae0c69ea1ddaca95fb96
Stefan Sundin <git@stefansundin.com>
2023-09-19 22:52:37 -0700

    Update dark mode code. Bootstrap v5.3.2.

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,43 +1,56 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const checkbox = document.getElementById("dark-mode");
-  checkbox.addEventListener("click", (e) => {
-    if (e.isTrusted) {
-      // user initiated
-      if (e.shiftKey) {
-        localStorage.removeItem("dark-mode");
-        e.target.checked = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        e.target.indeterminate = true;
+let localStorageUsable = false;
+try {
+  if ('localStorage' in window && localStorage) {
+    localStorageUsable = true;
+  }
+} catch {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const checkbox = document.getElementById('dark-mode');
+  const label = checkbox.parentElement.querySelector('label[for="dark-mode"]');
+  checkbox.parentElement.classList.remove('d-none'); // Only show dark mode switch if JavaScript is enabled
+  for (const el of [checkbox, label]) {
+    el.addEventListener('click', (e) => {
+      if (e.isTrusted && localStorageUsable) {
+        // user initiated
+        if (e.shiftKey) {
+          localStorage.removeItem('theme');
+          checkbox.checked = window.matchMedia('(prefers-color-scheme: dark)').matches;
+          checkbox.indeterminate = true;
+        }
+        else {
+          localStorage.setItem('theme', (checkbox.checked ? 'dark' : 'light'));
+        }
       }
-      else {
-        localStorage.setItem("dark-mode", e.target.checked.toString());
+
+      const theme = (checkbox.checked ? 'dark' : 'light');
+      document.documentElement.setAttribute('data-bs-theme', theme);
+
+      const launcher = document.getElementById('img-launcher');
+      if (launcher) {
+        launcher.src = `img/launcher-${theme}.png`;
       }
-    }
+    });
+  }
 
-    if (e.target.checked) {
-      document.body.classList.add("bg-dark");
-      document.body.classList.add("text-light");
+  // Pass theme=dark in the query string to default to dark mode
+  let theme = window.location.search.substring(1).split('&').find(v => v.startsWith('theme='))?.split('=')?.[1]
+  if (localStorageUsable) {
+    // localStorage has preference over query parameter
+    const localTheme = localStorage.getItem('theme');
+    if (localTheme) {
+      theme = localTheme;
     }
-    else {
-      document.body.classList.remove("bg-dark");
-      document.body.classList.remove("text-light");
-    }
-
-    const launcher = document.getElementById("img-launcher");
-    if (launcher) {
-      launcher.src = `img/launcher-${e.target.checked ? "dark":"light"}.png`;
-    }
-  });
-
-  const dark_mode = localStorage.getItem("dark-mode");
-  if (dark_mode == "true" || (dark_mode == undefined && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  }
+  if (theme === 'dark' || (theme === undefined && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     checkbox.click();
   }
-  checkbox.indeterminate = (dark_mode == undefined);
+  checkbox.indeterminate = (theme === undefined);
 });
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
-  const checkbox = document.getElementById("dark-mode");
-  if (checkbox.indeterminate && checkbox.checked != e.matches) {
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+  const checkbox = document.getElementById('dark-mode');
+  if (checkbox.indeterminate && checkbox.checked !== e.matches) {
     checkbox.click();
     checkbox.indeterminate = true;
   }
