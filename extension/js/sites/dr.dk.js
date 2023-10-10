3997cb42e2f4916331f65fa61794ba4db4ef7d95
Stefan Sundin <git@stefansundin.com>
2023-10-09 22:53:07 -0700

    Detect subtitles on dr.dk.

--- extension/js/sites/dr.dk.js
+++ extension/js/sites/dr.dk.js
@@ -1,11 +1,18 @@
 // https://www.dr.dk/drtv/se/hjernemassage-med-jan-hellesoee_-dagens-gaest-er-thomas-eje_242600
 // https://isl.dr-massive.com/api/account/items/242600/videos?delivery=stream&device=web_browser&ff=idp%2Cldp%2Crpt&lang=da&resolution=HD-1080&sub=Anonymous
 
-import { api_error, options, update_cmd, update_filename } from '../popup.js';
-import { $, get_json } from '../utils.js';
+import {
+  api_error,
+  options,
+  subtitles,
+  update_cmd,
+  update_filename,
+} from '../popup.js';
+import { $, extract_filename, get_json } from '../utils.js';
 
 function dr_dk_callback(streams) {
   const dropdown = $('#streams');
+
   for (const stream of streams) {
     const option = document.createElement('option');
     option.value = stream.url;
@@ -14,6 +21,19 @@ function dr_dk_callback(streams) {
     );
     dropdown.appendChild(option);
   }
+
+  for (const stream of streams) {
+    if (stream.subtitles) {
+      subtitles.push(...stream.subtitles.map((sub) => sub.link));
+      for (const sub of stream.subtitles) {
+        const option = document.createElement('option');
+        option.value = sub.link;
+        option.appendChild(document.createTextNode(extract_filename(sub.link)));
+        dropdown.appendChild(option);
+      }
+    }
+  }
+
   update_cmd();
 }
 
