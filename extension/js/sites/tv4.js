stefansundin/privatkopiera

commit b3f102837d377735a4494eaca64d7c9639d57b43

Rename lots of functions and variables. Delete some unused code.

extension/js/sites/tv4.js
@@ -17,12 +17,12 @@
 // https://playback2.a2d.tv/play/8ff56dd8e591e1854af1?service=tv4play&device=browser&protocol=hls%2Cdash&drm=widevine&browser=GoogleChrome&capabilities=live-drm-adstitch-2%2Cyospace3
 
 import {
-  api_error,
+  apiError,
   info,
   options,
   processPlaylist,
-  update_cmd,
-  update_filename,
+  updateCommand,
+  updateFilename,
 } from '../popup.js';
 import {
   $,
@@ -33,34 +33,34 @@ import {
   tab,
 } from '../utils.js';
 
-function tv4play_media_callback(data, expand = false) {
+function callback(data, expand = false) {
   // console.debug('data', data);
   const dropdown = $('#streams');
-  let fn = data.metadata.title.trim();
+  let filename = data.metadata.title.trim();
   if (options.add_source_id_to_filename && data.id) {
-    fn += ` [TV4 ${data.id}]`;
+    filename += ` [TV4 ${data.id}]`;
   }
-  fn += `.${options.default_video_file_extension}`;
+  filename += `.${options.default_video_file_extension}`;
   if (dropdown.childNodes.length === 0) {
-    update_filename(fn);
+    updateFilename(filename);
   }
 
   const option = document.createElement('option');
   option.value = data.playbackItem.manifestUrl;
   option.appendChild(document.createTextNode(data.metadata.title.trim()));
-  option.setAttribute('data-filename', fn);
+  option.setAttribute('data-filename', filename);
   dropdown.appendChild(option);
-  update_cmd();
+  updateCommand();
 
   if (expand && data.playbackItem.type === 'hls') {
-    processPlaylist(data.playbackItem.manifestUrl, data.contentDuration).catch(
-      api_error,
+    processPlaylist(data.playbackItem.manifestUrl).catch(
+      apiError,
     );
   }
 }
 
-function tv4_error(err) {
-  console.log(err);
+function error(err) {
+  console.error(err);
   // delete the cached access token in case it needs refreshing.. the user can try again and maybe it will work.
   localStorage.removeItem('tv4-access-token');
   info('Något gick fel. Videon kanske kräver att du är inloggad?');
@@ -70,26 +70,26 @@ export default [
   {
     re: /^https?:\/\/(?:www\.)?tv4play\.se\.?\/(?:video|program|klipp|korthet)\/([0-9a-f]+)/,
     func: async (ret) => {
-      const video_id = ret[1];
-      update_filename(`${video_id}.${options.default_video_file_extension}`);
+      const videoId = ret[1];
+      updateFilename(`${videoId}.${options.default_video_file_extension}`);
 
-      let access_token = localStorageGetWithExpiry('tv4-access-token');
-      if (access_token && !access_token.startsWith('ey')) {
-        access_token = undefined;
+      let accessToken = localStorageGetWithExpiry('tv4-access-token');
+      if (accessToken && !accessToken.startsWith('ey')) {
+        accessToken = undefined;
       }
 
-      if (!access_token) {
+      if (!accessToken) {
         try {
           const injectionResult = await chrome.scripting.executeScript({
             target: { tabId: tab.id },
             func: async () => {
               try {
-                const refresh_token = document.cookie
+                const refreshToken = document.cookie
                   .split(';')
                   .map((c) => c.trim())
                   .find((c) => c.startsWith('tv4-refresh-token='))
                   ?.split('=')[1];
-                if (!refresh_token) {
+                if (!refreshToken) {
                   return { error: 'no refresh token' };
                 }
                 const response = await fetch(
@@ -103,7 +103,7 @@ export default [
                       'content-type': 'application/json',
                     },
                     body: JSON.stringify({
-                      refresh_token,
+                      refreshToken,
                       client_id: 'tv4-web',
                     }),
                   },
@@ -115,8 +115,8 @@ export default [
                     } ${await response.text()}`,
                   };
                 }
-                const access_token_data = await response.json();
-                return { result: access_token_data.access_token };
+                const accessTokenData = await response.json();
+                return { result: accessTokenData.accessToken };
               } catch (err) {
                 return { error: err.message };
               }
@@ -130,10 +130,10 @@ export default [
           } else if (injectionResult[0].result.error) {
             throw new Error(injectionResult[0].result.error);
           }
-          access_token = injectionResult[0].result.result;
+          accessToken = injectionResult[0].result.result;
           localStorageSetWithExpiry(
             'tv4-access-token',
-            access_token,
+            accessToken,
             4 * 3600 * 1000,
           );
         } catch (err) {
@@ -142,36 +142,36 @@ export default [
         }
       }
 
-      const metadata_url = `https://playback2.a2d.tv/play/${video_id}?service=tv4play&device=browser&protocol=hls%2Cdash&drm=widevine&browser=GoogleChrome&capabilities=live-drm-adstitch-2%2Cyospace3`;
-      fetchJson(metadata_url, {
-        headers: access_token
+      const metadataUrl = `https://playback2.a2d.tv/play/${videoId}?service=tv4play&device=browser&protocol=hls%2Cdash&drm=widevine&browser=GoogleChrome&capabilities=live-drm-adstitch-2%2Cyospace3`;
+      fetchJson(metadataUrl, {
+        headers: accessToken
           ? {
-              'X-Jwt': `Bearer ${access_token}`,
+              'X-Jwt': `Bearer ${accessToken}`,
             }
           : {},
       })
-        .then((data) => tv4play_media_callback(data, true))
-        .catch(tv4_error);
+        .then((data) => callback(data, true))
+        .catch(error);
     },
   },
   {
     re: /^https?:\/\/(?:www\.)?tv4\.se\.?\//,
-    func: async (ret, url) => {
+    func: async (_, url) => {
       const data = await fetchPageData(url);
       const videoIds = Object.values(data.props.apolloState)
         .filter((thing) => thing.type === 'clipvideo')
         .map((v) => v.id);
 
       for (const videoId of videoIds) {
-        const metadata_url = `https://playback2.a2d.tv/play/${videoId}?service=tv4play&device=browser&protocol=hls%2Cdash&drm=widevine&browser=GoogleChrome&capabilities=live-drm-adstitch-2%2Cyospace3`;
-        console.log(metadata_url);
-        fetchJson(metadata_url, {
+        const metadataUrl = `https://playback2.a2d.tv/play/${videoId}?service=tv4play&device=browser&protocol=hls%2Cdash&drm=widevine&browser=GoogleChrome&capabilities=live-drm-adstitch-2%2Cyospace3`;
+        console.log(metadataUrl);
+        fetchJson(metadataUrl, {
           headers: {
             accept: 'application/json',
           },
         })
-          .then(tv4play_media_callback)
-          .catch(tv4_error);
+          .then(callback)
+          .catch(error);
       }
 
       if (videoIds.length === 0) {

extension/js/popup.js
@@ -7,8 +7,8 @@ import tv4 from './sites/tv4.js';
 import ur from './sites/ur.js';
 import {
   $,
-  extract_extension,
-  extract_filename,
+  extractExtension,
+  extractFilename,
   fetchText,
   getTab,
   isAndroid,
@@ -39,9 +39,9 @@ export const subtitles = [];
 let url, site;
 let initialFilenameSet = false;
 
-export function update_filename(fn) {
+export function updateFilename(filename) {
   // replace illegal characters
-  $('#filename').value = fn
+  $('#filename').value = filename
     .replace(/[/\\:]/g, '-')
     .replace(/["”´‘’]/g, "'")
     .replace(/[*?<>|!]/g, '')
@@ -54,7 +54,7 @@ export function info(text) {
   el.textContent = text;
 }
 
-export function api_error(e) {
+export function apiError(e) {
   console.error(e);
   const el = $('#info');
   while (el.hasChildNodes()) {
@@ -99,7 +99,7 @@ function download_info(program) {
   el.appendChild(document.createTextNode(`.`));
 }
 
-export function update_cmd(e) {
+export function updateCommand(e) {
   const filename = $('#filename');
   const streams = $('#streams');
   const stream = streams.selectedOptions[0];
@@ -114,9 +114,9 @@ export function update_cmd(e) {
     (filename.value === '' && !initialFilenameSet)
   ) {
     initialFilenameSet = true;
-    const fn = stream.getAttribute('data-filename');
-    if (fn) {
-      update_filename(fn);
+    const filename = stream.getAttribute('data-filename');
+    if (filename) {
+      updateFilename(filename);
     }
   }
 
@@ -132,47 +132,47 @@ export function update_cmd(e) {
   }
 
   let output_path = options.output_path + filename.value;
-  const ext = extract_extension(filename.value);
-  const stream_fn = extract_filename(url);
-  const stream_ext = extract_extension(url);
-  streams.title = stream_fn;
-  if (stream_ext === 'f4m') {
+  const extension = extractExtension(filename.value);
+  const streamFilename = extractFilename(url);
+  const streamExtension = extractExtension(url);
+  streams.title = streamFilename;
+  if (streamExtension === 'f4m') {
     cmd.value = `php AdobeHDS.php --delete --manifest "${url}" --outfile "${output_path}"`;
-  } else if (stream_ext === 'm4a' || stream_ext === 'mp3') {
+  } else if (streamExtension === 'm4a' || streamExtension === 'mp3') {
     cmd.value = url;
     $('#copy').classList.add('d-none');
     $('#download').classList.remove('d-none');
     const label = $("label[for='cmd']")[0];
     label.textContent = 'URL';
-  } else if (stream_fn.endsWith('_a.m3u8') || ext === 'mka' || ext === 'aac') {
-    if (ext === 'mkv') {
+  } else if (streamFilename.endsWith('_a.m3u8') || extension === 'mka' || extension === 'aac') {
+    if (extension === 'mkv') {
       output_path = output_path.replace(/\.mkv$/, '.mka');
-    } else if (ext === 'mp4') {
+    } else if (extension === 'mp4') {
       output_path = output_path.replace(/\.mp4$/, '.m4a');
     }
     cmd.value = `${options.ffmpeg_command} -i "${
       audio_stream || url
     }" -vn -c:a copy "${output_path}"`;
-  } else if (ext === 'm4a') {
+  } else if (extension === 'm4a') {
     cmd.value = `${options.ffmpeg_command} -i "${
       audio_stream || url
     }" -vn -c:a copy -bsf:a aac_adtstoasc "${output_path}"`;
-  } else if (ext === 'mp3' || ext === 'ogg') {
+  } else if (extension === 'mp3' || extension === 'ogg') {
     cmd.value = `${options.ffmpeg_command} -i "${
       audio_stream || url
     }" -vn "${output_path}"`;
-  } else if (stream_ext === 'vtt') {
-    if (ext === 'mkv' || ext === 'mp4') {
+  } else if (streamExtension === 'vtt') {
+    if (extension === 'mkv' || extension === 'mp4') {
       output_path = output_path.replace(/\.(mkv|mp4)$/, '.srt');
-    } else if (ext !== 'srt') {
+    } else if (extension !== 'srt') {
       output_path += '.srt';
     }
     cmd.value = `${options.ffmpeg_command} -i "${url}" "${output_path}"`;
   } else if (
     subtitles.length > 0 &&
-    (ext === 'srt' || ext === 'vtt' || url === subtitles[0])
+    (extension === 'srt' || extension === 'vtt' || url === subtitles[0])
   ) {
-    if (ext === 'mkv') {
+    if (extension === 'mkv') {
       output_path = output_path.replace(/\.mkv$/, '.srt');
     }
     cmd.value = `${options.ffmpeg_command} -i "${subtitles[0]}" "${output_path}"`;
@@ -182,7 +182,7 @@ export function update_cmd(e) {
       inputs.push(audio_stream);
     }
     inputs.push(...subtitles);
-    if (ext === 'mp4') {
+    if (extension === 'mp4') {
       cmd.value = `${options.ffmpeg_command} ${inputs
         .map((url) => `-i "${url}"`)
         .join(' ')} -c:v copy -c:a copy -bsf:a aac_adtstoasc "${output_path}"`;
@@ -204,7 +204,7 @@ export function update_cmd(e) {
   }
 }
 
-export async function processPlaylist(url, mediaDuration) {
+export async function processPlaylist(url) {
   if (isAndroid) {
     return;
   }
@@ -293,12 +293,12 @@ export async function processPlaylist(url, mediaDuration) {
     const option = document.createElement('option');
     option.value = baseUrl + ext_x_media['AUDIO']['URI'];
     option.appendChild(
-      document.createTextNode(extract_filename(ext_x_media['AUDIO']['URI'])),
+      document.createTextNode(extractFilename(ext_x_media['AUDIO']['URI'])),
     );
     dropdown.insertBefore(option, default_option);
   }
   dropdown.getElementsByTagName('option')[0].selected = true;
-  update_cmd();
+  updateCommand();
 }
 
 async function call_func() {
@@ -422,8 +422,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     }).observe(cmd);
   }
 
-  $('#streams').addEventListener('input', update_cmd);
-  $('#filename').addEventListener('input', update_cmd);
+  $('#streams').addEventListener('input', updateCommand);
+  $('#filename').addEventListener('input', updateCommand);
 
   if (isFirefox && !isAndroid) {
     document

extension/js/sites/dr.dk.js
@@ -4,12 +4,12 @@
 import {
   options,
   subtitles,
-  update_cmd,
-  update_filename,
+  updateCommand,
+  updateFilename,
 } from '../popup.js';
-import { $, extract_filename, fetchJson, tab } from '../utils.js';
+import { $, extractFilename, fetchJson, tab } from '../utils.js';
 
-function dr_dk_callback(streams) {
+function callback(streams) {
   const dropdown = $('#streams');
 
   for (const stream of streams) {
@@ -27,21 +27,21 @@ function dr_dk_callback(streams) {
       for (const sub of stream.subtitles) {
         const option = document.createElement('option');
         option.value = sub.link;
-        option.appendChild(document.createTextNode(extract_filename(sub.link)));
+        option.appendChild(document.createTextNode(extractFilename(sub.link)));
         dropdown.appendChild(option);
       }
     }
   }
 
-  update_cmd();
+  updateCommand();
 }
 
 export default [
   {
     re: /^https?:\/\/(?:www\.)?dr\.dk\.?\/.*_(\d+)/,
     func: async (ret) => {
-      const video_id = ret[1];
-      console.log('video_id', video_id);
+      const videoId = ret[1];
+      console.log('videoId', videoId);
 
       // Grab the page title and the required token from the page's localStorage
       const injectionResult = await chrome.scripting.executeScript({
@@ -60,20 +60,20 @@ export default [
       console.log('tokens', tokens);
       const token = tokens.find((t) => t.type === 'UserAccount').value;
 
-      const data_url = `https://production.dr-massive.com/api/account/items/${video_id}/videos?delivery=stream&device=web_browser&ff=idp%2Cldp%2Crpt&lang=da&resolution=HD-1080&sub=Anonymous`;
-      console.log(data_url);
+      const dataUrl = `https://production.dr-massive.com/api/account/items/${videoId}/videos?delivery=stream&device=web_browser&ff=idp%2Cldp%2Crpt&lang=da&resolution=HD-1080&sub=Anonymous`;
+      console.log(dataUrl);
       if (options.add_source_id_to_filename) {
-        title += ` [DR ${video_id}]`;
+        title += ` [DR ${videoId}]`;
       }
-      update_filename(`${title}.${options.default_video_file_extension}`);
+      updateFilename(`${title}.${options.default_video_file_extension}`);
 
-      const streams = await fetchJson(data_url, {
+      const streams = await fetchJson(dataUrl, {
         headers: {
           accept: 'application/json',
           'x-authorization': `Bearer ${token}`,
         },
       });
-      await dr_dk_callback(streams);
+      callback(streams);
     },
   },
 ];

extension/js/sites/nrk.js
@@ -17,34 +17,32 @@
 // https://podkast.nrk.no/fil/bjoernen_lyver/bjoernen_lyver_2018-02-16_1300_760.MP3
 
 import {
-  api_error,
+  apiError,
   options,
   processPlaylist,
   subtitles,
-  update_cmd,
-  update_filename,
+  updateCommand,
+  updateFilename,
 } from '../popup.js';
 import {
   $,
-  extract_filename,
+  extractFilename,
   fetchJson,
   fetchPageData,
   getDocumentTitle,
-  parse_pt,
 } from '../utils.js';
 
-async function nrk_callback(data) {
+async function callback(data) {
   console.log(data);
 
   const streams = $('#streams');
-  const duration = parse_pt(data.playable.duration);
   for (const asset of data.playable.assets) {
     const option = document.createElement('option');
     option.value = asset.url;
-    option.appendChild(document.createTextNode(extract_filename(asset.url)));
+    option.appendChild(document.createTextNode(extractFilename(asset.url)));
     streams.appendChild(option);
 
-    processPlaylist(asset.url, duration).catch(api_error);
+    processPlaylist(asset.url).catch(apiError);
   }
 
   for (const subtitle of data.playable.subtitles) {
@@ -58,9 +56,9 @@ async function nrk_callback(data) {
     streams.appendChild(option);
   }
 
-  let ext = options.default_video_file_extension;
+  let extension = options.default_video_file_extension;
   if (data.sourceMedium === 'audio') {
-    ext = options.default_audio_file_extension;
+    extension = options.default_audio_file_extension;
   }
   let title = data?.statistics?.qualityOfExperience?.title;
   if (!title) {
@@ -73,26 +71,26 @@ async function nrk_callback(data) {
     title += ` [NRK ${data.id}]`;
   }
   console.log('title', title);
-  const fn = `${title}.${ext}`;
-  update_filename(fn);
-  update_cmd();
+  const filename = `${title}.${extension}`;
+  updateFilename(filename);
+  updateCommand();
 }
 
-function nrk_podcast_callback(data) {
+function podcastCallback(data) {
   console.log(data);
   const streams = $('#streams');
   for (const [i, asset] of data.playable.assets.entries()) {
-    const fn = extract_filename(asset.url);
+    const filename = extractFilename(asset.url);
     if (i === 0) {
-      update_filename(fn);
+      updateFilename(filename);
     }
     const option = document.createElement('option');
     option.value = asset.url;
-    option.setAttribute('data-filename', fn);
-    option.appendChild(document.createTextNode(fn));
+    option.setAttribute('data-filename', filename);
+    option.appendChild(document.createTextNode(filename));
     streams.appendChild(option);
   }
-  update_cmd();
+  updateCommand();
 }
 
 export default [
@@ -100,15 +98,15 @@ export default [
     re: /^https?:\/\/radio\.nrk\.no\.?\/serie[^A-Z]*\/([A-Z][A-Z0-9]+)/,
     func: async (ret) => {
       const id = ret[1];
-      const data_url = `https://psapi.nrk.no/playback/manifest/program/${id}`;
-      update_filename(`${id}.${options.default_audio_file_extension}`);
-      console.log(data_url);
-      const data = await fetchJson(data_url, {
+      const dataUrl = `https://psapi.nrk.no/playback/manifest/program/${id}`;
+      updateFilename(`${id}.${options.default_audio_file_extension}`);
+      console.log(dataUrl);
+      const data = await fetchJson(dataUrl, {
         headers: {
           accept: 'application/json',
         },
       });
-      await nrk_callback(data);
+      await callback(data);
     },
   },
   {
@@ -117,34 +115,34 @@ export default [
       // https://radio.nrk.no/podkast/bjoernen_lyver/l_709fe866-13a5-498d-9fe8-6613a5d98d1f
       // https://psapi.nrk.no/playback/metadata/l_709fe866-13a5-498d-9fe8-6613a5d98d1f
       // https://psapi.nrk.no/playback/manifest/podcast/l_68cb20c7-5a8c-4031-8b20-c75a8c003183
-      const data_url = `https://psapi.nrk.no/playback/manifest/podcast/${ret[1]}`;
-      console.log(data_url);
+      const dataUrl = `https://psapi.nrk.no/playback/manifest/podcast/${ret[1]}`;
+      console.log(dataUrl);
 
-      const data = await fetchJson(data_url, {
+      const data = await fetchJson(dataUrl, {
         headers: {
           accept: 'application/json',
           // This is what the website normally sends:
           // accept: 'application/vnd.nrk.psapi+json; version=9; player=radio-web-player; device=player-core',
         },
       });
-      await nrk_podcast_callback(data);
+      await podcastCallback(data);
     },
   },
   {
     re: /^https?:\/\/radio\.nrk\.no\.?\/serie\/.+\/(l_[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b)/,
     func: async (ret) => {
       // https://radio.nrk.no/serie/tett-paa-norske-artister/sesong/2018/MYNF51000518
-      const data_url = `https://psapi.nrk.no/playback/manifest/program/${ret[1]}`;
-      console.log(data_url);
+      const dataUrl = `https://psapi.nrk.no/playback/manifest/program/${ret[1]}`;
+      console.log(dataUrl);
 
-      const data = await fetchJson(data_url, {
+      const data = await fetchJson(dataUrl, {
         headers: {
           accept: 'application/json',
           // This is what the website normally sends:
           // accept: 'application/vnd.nrk.psapi+json; version=9; player=radio-web-player; device=player-core',
         },
       });
-      await nrk_podcast_callback(data);
+      await podcastCallback(data);
     },
   },
   {
@@ -160,8 +158,8 @@ export default [
       }
 
       const dataUrl = `https://psapi.nrk.no/playback/manifest/program/${prfId}`;
-      console.log('dataUrl', dataUrl);
-      update_filename(`${prfId}.${options.default_video_file_extension}`);
+      console.log(dataUrl);
+      updateFilename(`${prfId}.${options.default_video_file_extension}`);
 
       const data = await fetchJson(dataUrl, {
         headers: {
@@ -170,7 +168,7 @@ export default [
           // accept: '*/*',
         },
       });
-      await nrk_callback(data);
+      await callback(data);
     },
   },
 ];

extension/js/sites/sverigesradio.js
@@ -10,23 +10,23 @@
 // Get audio URL:
 // https://www.sverigesradio.se/sida/playerajax/getaudiourl?id=5678841&type=clip&quality=high&format=iis
 
-import { info, options, update_cmd } from '../popup.js';
-import { $, extract_extension, fetchJson, tab } from '../utils.js';
+import { info, options, updateCommand } from '../popup.js';
+import { $, extractExtension, fetchJson, tab } from '../utils.js';
 
-function sr_callback(stream, data) {
+function callback(stream, data) {
   const dropdown = $('#streams');
-  const ext = extract_extension(data.audioUrl) || 'mp3';
+  const extension = extractExtension(data.audioUrl) || 'mp3';
   const option = document.createElement('option');
   option.appendChild(document.createTextNode(stream.title));
   dropdown.appendChild(option);
   option.value = data.audioUrl;
-  let fn = stream.title;
+  let filename = stream.title;
   if (options.add_source_id_to_filename) {
-    fn += ` [SR ${stream.id}]`;
+    filename += ` [SR ${stream.id}]`;
   }
-  fn += `.${ext}`;
-  option.setAttribute('data-filename', fn);
-  update_cmd();
+  filename += `.${extension}`;
+  option.setAttribute('data-filename', filename);
+  updateCommand();
 }
 
 export default [
@@ -56,14 +56,14 @@ export default [
                 header = header.parentNode;
               }
               let title = document.title;
-              const title_element =
+              const titleElement =
                 header.getElementsByClassName('main-audio-new__title')[0] ||
                 header.getElementsByClassName('related-audio__title')[0] ||
                 header.getElementsByClassName(
                   'article-audio-details__header-title',
                 )[0];
-              if (title_element) {
-                title = title_element.textContent.trim();
+              if (titleElement) {
+                title = titleElement.textContent.trim();
               } else {
                 const dash = title.lastIndexOf('-');
                 if (dash !== -1) {
@@ -93,14 +93,14 @@ export default [
       const streams = injectionResult[0].result.result;
 
       for (const stream of streams) {
-        const data_url = `/playerajax/getaudiourl?id=${stream.id}&type=${stream.type}&quality=high&format=iis`;
+        const dataUrl = `/playerajax/getaudiourl?id=${stream.id}&type=${stream.type}&quality=high&format=iis`;
 
-        const data = await fetchJson(data_url, {
+        const data = await fetchJson(dataUrl, {
           headers: {
             accept: 'application/json',
           },
         });
-        sr_callback(stream, data);
+        callback(stream, data);
       }
 
       if (streams.length === 0) {

commit 534113e6e9f0a7a56024cf981e55a727444ae8c1

Fix tv4play.se when logged in (broken by b3f102837d377735a4494eaca64d7c9639d57b43).

extension/js/sites/tv4.js
@@ -101,7 +101,7 @@ export default [
                       'content-type': 'application/json',
                     },
                     body: JSON.stringify({
-                      refreshToken,
+                      refresh_token: refreshToken,
                       client_id: 'tv4-web',
                     }),
                   },
@@ -110,7 +110,7 @@ export default [
                   return { error: `Invalid response: ${response.status} ${await response.text()}` };
                 }
                 const accessTokenData = await response.json();
-                return { result: accessTokenData.accessToken };
+                return { result: accessTokenData.access_token };
               } catch (err) {
                 return { error: err.message };
               }